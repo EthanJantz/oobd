23abbc4d9f6e759f465242a33c7dc22d2b6204a5
Florian Ragwitz <forian.ragwitz@gmail.com>
2025-08-31 07:44:04 +0000

    Scanner improvements
    Collect candidates for deletion, and don't walk into other file systems.

--- cmd/scan/main.go
+++ cmd/scan/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/ethanjantz/oobd/pkg/scan"
 )
 
 func main() {
-	scan.Dir("/home/", scan.DefaultOpts())
+	r, err := scan.Dir("/home/rafl", scan.DefaultOpts())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for p, m := range r {
+		fmt.Printf("%s %d\n", p, m.Size)
+	}
 }
--- pkg/scan/scan.go
+++ pkg/scan/scan.go
@@ -1,33 +1,57 @@
 package scan
 
 import (
-	"fmt"
-	"github.com/dundee/gdu/v5/pkg/analyze"
-	"github.com/dundee/gdu/v5/pkg/fs"
+	"maps"
 	"time"
+
+	"github.com/dundee/gdu/v5/pkg/analyze"
+	"github.com/dundee/gdu/v5/pkg/device"
+	"github.com/dundee/gdu/v5/pkg/fs"
 )
 
-func Dir(p string, o Opts) {
+type Result map[string]Meta
+
+type Meta struct {
+	Mtime time.Time
+	Size  int64
+}
+
+func Dir(p string, o Opts) (Result, error) {
 	// parallel for now, just for quicker development
 	// should be Seq for less resource churn
 	//s := analyze.CreateSeqAnalyzer()
 	s := analyze.CreateAnalyzer()
-	r := s.AnalyzeDir(p, ignore, true)
+	ms, err := device.Getter.GetMounts()
+	if err != nil {
+		return nil, err
+	}
+	r := s.AnalyzeDir(p, ignore(device.GetNestedMountpointsPaths(p, ms)), true)
 	<-s.GetDone()
 	r.UpdateStats(fs.HardLinkedItems{})
-	walk(o.now.Add(-o.minAge), r, o, 2)
+	return wannaDel(o.now.Add(-o.minAge), r, o), nil
 }
 
-func walk(t time.Time, f fs.Item, o Opts, n uint) {
-	s := f.GetSize()
-	if s > o.minSize && f.GetMtime().Before(t) {
-		fmt.Printf("%s %d %d\n", f.GetPath(), f.GetUsage(), s)
-	}
-	if n > 0 {
-		for _, c := range f.GetFiles() {
-			walk(t, c, o, n-1)
+func wannaDel(t time.Time, r fs.Item, o Opts) Result {
+	type rec func(rec, fs.Item) Result
+	walk := func(walk rec, f fs.Item) Result {
+		p, mt, s := f.GetPath(), f.GetMtime(), f.GetSize()
+		if mt.Before(t) && s >= o.minSize && p != r.GetPath() {
+			return Result{p: Meta{mt, s}}
 		}
+		ret := Result{}
+		for _, c := range f.GetFiles() {
+			maps.Copy(ret, walk(walk, c))
+		}
+		return ret
 	}
+
+	return walk(walk, r)
 }
 
-func ignore(name, p string) bool { return false }
+func ignore(ps []string) func(string, string) bool {
+	ign := map[string]struct{}{}
+	for _, p := range ps {
+		ign[p] = struct{}{}
+	}
+	return func(_, p string) bool { _, ok := ign[p]; return ok }
+}
